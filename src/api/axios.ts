8ffb089a2068a6850c8a824d3ce8d69f20665860
Zeeshan <zeeshan@guiker.com>
2020-07-21 10:19:44 +0530

    add fixes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL: string = `https://covid19.mathdro.id/api`;
+export const BASE_URL: string = `https://covid19.mathdro.id/api`;
 
 export default axios.create({
   baseURL: BASE_URL,
